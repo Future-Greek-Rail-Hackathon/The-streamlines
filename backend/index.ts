003ad9b6593cacb5665292a2c6e3d3ffe3d35a15
parhsmaropoulos <parhsmaro13@gmail.com>
2021-06-28 15:20:32 +0300

    bug fixed

--- backend/index.ts
+++ backend/index.ts
@@ -25,7 +25,7 @@ app.use((req, res, next) => {
 
 // Connect to database
 pool.connect(function (err, client, done) {
-  if (err) throw new Error(err);
+  if (err) throw new Error(err.message);
   console.log('Connected');
 });
 
