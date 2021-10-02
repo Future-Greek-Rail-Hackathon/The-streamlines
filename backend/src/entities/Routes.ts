7c23f0b40671ce99e03f7d35428c9da2b36e9b73
paris maropoulos <pmaropoulos@skroutzdelivery.gr>
2021-10-02 23:32:35 +0300

    Route model

---
+++ backend/src/entities/Routes.ts
@@ -0,0 +1,41 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  Index,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from "typeorm";
+import { TrainStop } from "./TrainStop";
+
+@Entity({ name: "routes" })
+export class Route {
+  @PrimaryGeneratedColumn()
+  id?: number;
+
+  @Index()
+  @Column({ nullable: true })
+  startLocation: TrainStop;
+
+  @Index()
+  @Column({ nullable: true })
+  endLocation: TrainStop;
+
+  @Column({ nullable: true, type: "timestamp" })
+  estimatedTime: Date;
+
+  @Column({ nullable: true })
+  destination: TrainStop;
+
+  @Column({ nullable: true, type: "timestamp" })
+  startTime: Date;
+
+  @Column({ nullable: true, type: "timestamp" })
+  endTime: Date;
+
+  @CreateDateColumn({ type: "timestamp" })
+  createdAt?: Date;
+
+  @UpdateDateColumn({ type: "timestamp" })
+  updatedAt: Date;
+}
