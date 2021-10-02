7a278be41b9c99cf46b90b626c2db8e0cc151d16
paris maropoulos <pmaropoulos@skroutzdelivery.gr>
2021-10-03 00:18:43 +0300

    train service

---
+++ backend/src/services/train.ts
@@ -0,0 +1,25 @@
+import moment from 'moment-timezone';
+import { getRepository, Repository } from 'typeorm';
+import { Train, TrainType } from '../entity/Trains';
+moment.tz.setDefault('Europe/Athens');
+
+export class TrainService {
+  trainRepository: Repository<Train>;
+
+  constructor() {
+    this.trainRepository = getRepository(Train);
+  }
+
+  async createTrain(type: TrainType, canGoAboard: boolean, maxWeight: number): Promise<Train> {
+    let newTrain = this.trainRepository.create();
+    newTrain.type = type;
+    newTrain.canGoAboard = canGoAboard;
+    newTrain.maxWeight = type === TrainType.FULL_TRAIN ? 750 : 0;
+
+    return await this.trainRepository.save(newTrain);
+  }
+
+  findById(trainId: number) {
+    return this.trainRepository.find({ id: trainId });
+  }
+}
