b3b077c721f474cca8db6d584f0553cb9e7d19e2
paris maropoulos <pmaropoulos@skroutzdelivery.gr>
2021-10-03 14:44:48 +0300

    Add userHandler to routes file

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,6 +1,7 @@
-import { Router } from "express";
+import { Router } from 'express';
+import userHandler from './controller/userHandler';
 const router: Router = Router();
 
-// router.use('/users', userRouter);
+router.use('/users', userHandler);
 
 export default router;
