b5eee987c53cbcbb5972804c1d7d7ad5d8e73426
paris maropoulos <pmaropoulos@skroutzdelivery.gr>
2021-10-03 14:51:06 +0300

    Train Handler: post endpoint

---
+++ backend/src/controller/trainHandler.ts
@@ -0,0 +1,23 @@
+import { NextFunction, Request, Response, Router } from 'express';
+import { TrainType } from '../entity/Trains';
+import catchErrors from '../lib/catchErrors';
+import { TrainService } from '../services/train';
+const trainHandler: Router = Router();
+
+trainHandler
+  .route('/') // post /api/train/
+  .post(
+    catchErrors(async (req: Request, res: Response, next: NextFunction) => {
+      const type = req.body['type'];
+      const canGoAboard = req.body['canGoAboard'];
+      const maxWeight = req.body['maxWeight'];
+
+      let newType = type === 'full_train' ? TrainType.FULL_TRAIN : TrainType.NON_FULL_TRAIN;
+
+      const trainModel = new TrainService();
+      const newTrain = await trainModel.createTrain(newType, canGoAboard, maxWeight);
+      res.json(newTrain);
+    }),
+  );
+
+export default trainHandler;
--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express';
+import trainHandler from './controller/trainHandler';
 import userHandler from './controller/userHandler';
 const router: Router = Router();
 
 router.use('/users', userHandler);
+router.use('/trains', trainHandler);
 
 export default router;
