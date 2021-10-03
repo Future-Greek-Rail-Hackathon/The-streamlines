7ae438a3b6a0a5f4d8cad270f55c6147c37e3868
paris maropoulos <pmaropoulos@skroutzdelivery.gr>
2021-10-03 14:44:31 +0300

    Update gitignore and app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,22 +1,22 @@
-import _ from "./lib/_dotenv";
+import _ from './lib/_dotenv';
 console.log(_);
-import express from "express";
-import cors from "cors";
+import express from 'express';
+import cors from 'cors';
 // import { packageRouter, clusterRouter } from "./models/packages.routers";
 // import { driverRouter } from "./models/driver.routers";
-import cookieParser from "cookie-parser";
+import cookieParser from 'cookie-parser';
+import router from './routes';
 
-const bodyParser = require("body-parser");
+const bodyParser = require('body-parser');
 const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
+app.use(express.urlencoded({ extended: true }));
 
 // Set router base paths
-// app.use("/api/packages", packageRouter);
-// app.use("/api/drivers", driverRouter);
-// app.use("/api/clusters", clusterRouter);
+app.use('/api/', router);
 
 export default app;
