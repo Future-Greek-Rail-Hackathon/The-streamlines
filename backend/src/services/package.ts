ccb7f71afdfb12f5dab05cc748aa0c0442a9cfb4
paris maropoulos <pmaropoulos@skroutzdelivery.gr>
2021-10-06 17:47:37 +0300

    Rename file

--- backend/src/services/package.ts
+++ backend/src/services/package.ts
@@ -1,11 +1,6 @@
 import moment from 'moment-timezone';
 import { getRepository, Repository } from 'typeorm';
-import { Order } from '../entity/Order';
 import { Package } from '../entity/Package';
-import { Train, TrainType } from '../entity/Trains';
-import { RouteService } from './route';
-import { TrainService } from './train';
-import { UserService } from './user';
 moment.tz.setDefault('Europe/Athens');
 
 export class PackageService {
@@ -15,9 +10,8 @@ export class PackageService {
     this.packageRepository = getRepository(Package);
   }
 
-  async createPackage(pckg: Package, order: Order): Promise<Package> {
+  async createPackage(pckg: Package): Promise<Package> {
     let newPackage = this.packageRepository.create(pckg);
-    newPackage.order = order;
 
     return await this.packageRepository.save(newPackage);
   }
