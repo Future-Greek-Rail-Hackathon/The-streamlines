b15bda3a70e951b426735b916817c8c66dfa5666
paris maropoulos <pmaropoulos@skroutzdelivery.gr>
2021-10-06 17:49:03 +0300

    Routes: assign wagon

--- backend/src/controller/trainHandler.ts
+++ backend/src/controller/trainHandler.ts
@@ -2,6 +2,7 @@ import { NextFunction, Request, Response, Router } from 'express';
 import { TrainType } from '../entity/Trains';
 import catchErrors from '../lib/catchErrors';
 import { TrainService } from '../services/train';
+import { WagonService } from '../services/wagons';
 const trainHandler: Router = Router();
 
 trainHandler
@@ -29,6 +30,41 @@ trainHandler
       const trainModel = new TrainService();
       const train = await trainModel.findById(id);
       res.json(train);
+    }),
+  );
+
+trainHandler
+  .route('/') // get /api/trains/
+  .get(
+    catchErrors(async (req: Request, res: Response, next: NextFunction) => {
+      const trainModel = new TrainService();
+      const trains = await trainModel.findAll();
+      res.json(trains);
+    }),
+  );
+
+trainHandler
+  .route('/assign_wagon') // post /api/trains/assign_wagon
+  .post(
+    catchErrors(async (req: Request, res: Response, next: NextFunction) => {
+      const wagonId = req.body['wagonId'];
+      const trainId = req.body['trainId'];
+
+      const trainModel = new TrainService();
+      const train = await trainModel.findById(trainId);
+
+      const wagonModel = new WagonService();
+      const wagon = await wagonModel.findById(wagonId);
+
+      if (train.wagons.includes(wagon)) {
+        res.end('already assigned');
+      } else if (wagon.currentTrain !== null) {
+        res.end('already got train');
+      }
+
+      let newTrain = await trainModel.addWagon(train, wagon);
+      await trainModel.trainRepository.save(newTrain);
+      res.json(newTrain);
     }),
   );
 
